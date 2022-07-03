3ea77bf4029c6e27c84a00bafb217fbd81845080
aishaabdul777 <aishaabdul777@gmail.com>
2022-07-03 13:01:51 -0700

    first commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
- * return: always 0 (Success)
+ * 
+ * Return: Always 0 (Success)
  */
 
 int main(void)
