68b702646d735599a713f5fc2dfab13fda43ce48
aishaabdul777 <aishaabdul777@gmail.com>
2022-07-06 06:32:47 -0700

    first commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
 			if (z > 9)
 			{
 				u = z % 10;
-				d = (z-u) / 10;
+				d = (z - u) / 10;
 
 				_putchar(44);
 				_putchar(32);
@@ -35,6 +35,7 @@ void times_table(void)
 					_putchar(32);
 					_putchar(32);
 				}
+
 				_putchar(z + '0');
 			}
 		}
